8f7051d4897737a885c14a209c42130eb5e3ad52
ChristianCav <christiancav55@gmail.com>
2024-09-23 14:36:35 -0400

    Added buttons to choose date and sport

---
+++ frontend/homepage.js
@@ -0,0 +1,22 @@
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
+
+let games = [];
+
+
+const today = dayjs();
+const formattedDate = today.format('YYYY-MM-DD')
+console.log(formattedDate)
+
+document.querySelector('.date-input').value = formattedDate
+
+let gamesHTML = '';
+
+games.forEach(game =>{
+    gamesHTML+= `
+    <div class="game-container">
+        <div class="team1-container">
+            <img class="team1-image" src="${game.image1}">
+        </div>
+    </div>    
+    `
+})
