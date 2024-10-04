9a635c51edaa0286f0ae88cd751505e16a1f393a
Your Namedwadwadadfwadwadhawhjduwahdgit config --global user.email you@example.comgit config --global user.name Your <drohi2026@gmail.com>
2024-10-03 21:59:07 -0400

    updates to fetch api

--- frontend/data/apirequest.js
+++ frontend/data/apirequest.js
@@ -1,9 +1,10 @@
-let url = 'http://127.0.0.1:5000/'
+let url = 'http://127.0.0.1:5000/data'
 
 // communicates with app.py to grab json file
-export function fetchApi(sport){
+export function fetchApi(sport, league){
     const params = new URLSearchParams();
     params.append('sport', sport);
+    params.append('league', league);
 
     const fullUrl = `${url}?${params.toString()}`;
 
@@ -16,4 +17,11 @@ export function fetchApi(sport){
         // return json file of sport information
         return response.json()
     })
+    .then(data => {
+        console.log(data);  // Log the received data
+        return data
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 }
